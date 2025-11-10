06bf2b89fe7049609f3c151ff473b5f552b52fec
LuizRojas <luizfrojas@gmail.com>
2025-11-09 22:38:52 -0300

    feat: adicionando opções restantes para o menu

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(){
 
     int opcao = 0;
     char nomeDigitado[100];
+    int tamDigitado = 0;
 
     do {
         printf("1. Nome Diretorio\n");
@@ -26,8 +27,7 @@ int main(){
         printf("10. Sair\n");
         scanf("%d", &opcao);
 
-        switch (opcao)
-        {
+        switch (opcao){
         case 1:
             nomeDiretorio(diretorioAtual);
             break;
@@ -37,26 +37,48 @@ int main(){
         case 3:
             printf("Digite o nome do diretorio que deseja abrir: ");
             scanf("%99s", nomeDigitado);
-
             diretorioAtual = abrirDiretorio(diretorioAtual, nomeDigitado);
             break;
         case 4:
+            diretorioAtual = voltarDiretorio(raiz, diretorioAtual);
             break;
         case 5:
+            printf("Digite o nome do novo diretorio: ");
+            scanf("%99s", nomeDigitado);
+            criarDiretorio(diretorioAtual, nomeDigitado);
             break;
         case 6:
+            printf("Digite o nome do diretorio para excluir: ");
+            scanf("%99s", nomeDigitado);
+            excluirDiretorio(diretorioAtual, nomeDigitado);
             break;
         case 7:
+            printf("Digite o nome do novo arquivo: ");
+            scanf("%99s", nomeDigitado);
+            printf("Digite o tamanho do arquivo: ");
+            scanf("%d", &tamDigitado);
+            criarArquivo(diretorioAtual, nomeDigitado, tamDigitado);
             break;
         case 8:
+            printf("Digite o nome do novo arquivo: ");
+            scanf("%99s", nomeDigitado);
+            printf("Digite o tamanho do arquivo: ");
+            scanf("%d", &tamDigitado);
+            criarArquivo(diretorioAtual, nomeDigitado, tamDigitado);
             break;
         case 9:
+            printf("Digite o nome do arquivo para excluir: ");
+            scanf("%99s", nomeDigitado);
+            excluirArquivo(diretorioAtual, nomeDigitado);
             break;
         case 10:
+            printf("Liberando toda a memoria...\n");
+            liberarDiretorio(raiz);
+            printf("Saindo do programa.\n");
             break;
+        default:
+            printf("Opcao invalida. Tente novamente.\n");
         }
     } while (opcao != 10);
-    
-
     return 0;
 }
